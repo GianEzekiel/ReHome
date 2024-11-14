c0d3c1c81ece5305e3c2b7d7430cd9ef1b87404a
KazuMoment <vencygyro@gmail.com>
2024-11-14 14:11:37 +0800

    made formatting for pet_details_page cleaner

--- lib/pet_details_page.dart
+++ lib/pet_details_page.dart
@@ -54,245 +54,244 @@ class PetDetailsPage extends StatelessWidget {
 
   Text petDescription() {
     return Text(
-                    pet.description,
-                    style: const TextStyle(
-                      fontSize: 14,
-                    ),
-                    textAlign: TextAlign.justify,
-                  );
+      pet.description,
+      style: const TextStyle(
+        fontSize: 14,
+      ),
+      textAlign: TextAlign.justify,
+    );
   }
 
   Align adoptButton() {
     return Align(
-                    alignment: Alignment.center,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 138, vertical: 20),
-                      ),
-                      child: const Text(
-                        "Adopt Me",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  );
+      alignment: Alignment.center,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          padding: const EdgeInsets.symmetric(horizontal: 138, vertical: 20),
+        ),
+        child: const Text(
+          "Adopt Me",
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 
   Row ownerDetails() {
     return Row(
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: AssetImage(pet.ownerIconPath),
-                        radius: 25,
-                      ),
-                      const SizedBox(width: 10),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            pet.ownerName,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '${pet.petName}\'s Owner',
-                            style: TextStyle(
-                              color: Colors.black.withOpacity(0.6),
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      SizedBox(
-                        height: 24,
-                        width: 24,
-                        child: IconButton(
-                          onPressed: (){},
-                          icon: const Icon(Icons.message_outlined),
-                        ),
-                      ),
-                    ],
-                  );
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage(pet.ownerIconPath),
+          radius: 25,
+        ),
+        const SizedBox(width: 10),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              pet.ownerName,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '${pet.petName}\'s Owner',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.6),
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+        const Spacer(),
+        SizedBox(
+          height: 24,
+          width: 24,
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.message_outlined),
+          ),
+        ),
+      ],
+    );
   }
 
   Row petDetails() {
     return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 75,
-                        width: 75,
-                        decoration: BoxDecoration(
-                            color: const Color(0xFFFFADC7),
-                            borderRadius: BorderRadius.circular(12)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(pet.sex,
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                            Text(
-                              'Sex',
-                              style: TextStyle(
-                                color: Colors.black.withOpacity(0.5),
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      Container(
-                        height: 75,
-                        width: 75,
-                        decoration: BoxDecoration(
-                            color: const Color(0xFFDEF3FD),
-                            borderRadius: BorderRadius.circular(12)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(pet.age,
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                            Text(
-                              'Age',
-                              style: TextStyle(
-                                color: Colors.black.withOpacity(0.5),
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      Container(
-                        height: 75,
-                        width: 75,
-                        decoration: BoxDecoration(
-                            color: const Color(0xFFBEFFF6),
-                            borderRadius: BorderRadius.circular(12)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(pet.weight,
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                            Text(
-                              'Weight',
-                              style: TextStyle(
-                                color: Colors.black.withOpacity(0.5),
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  );
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 75,
+          width: 75,
+          decoration: BoxDecoration(
+              color: const Color(0xFFFFADC7),
+              borderRadius: BorderRadius.circular(12)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(pet.sex,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                'Sex',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          width: 15,
+        ),
+        Container(
+          height: 75,
+          width: 75,
+          decoration: BoxDecoration(
+              color: const Color(0xFFDEF3FD),
+              borderRadius: BorderRadius.circular(12)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(pet.age,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                'Age',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          width: 15,
+        ),
+        Container(
+          height: 75,
+          width: 75,
+          decoration: BoxDecoration(
+              color: const Color(0xFFBEFFF6),
+              borderRadius: BorderRadius.circular(12)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(pet.weight,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                'Weight',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
   Row locationTitle() {
     return Row(
-                    children: [
-                      const Icon(
-                        Icons.location_on_outlined,
-                        color: Colors.blue,
-                        size: 16,
-                      ),
-                      const SizedBox(width: 5),
-                      Text(
-                        pet.location,
-                        style: TextStyle(
-                          color: Colors.black.withOpacity(0.6),
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  );
+      children: [
+        const Icon(
+          Icons.location_on_outlined,
+          color: Colors.blue,
+          size: 16,
+        ),
+        const SizedBox(width: 5),
+        Text(
+          pet.location,
+          style: TextStyle(
+            color: Colors.black.withOpacity(0.6),
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
   }
 
   Row petHeader() {
     return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        pet.petName,
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 24,
-                        width: 24,
-                        child: IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.favorite_border_outlined,
-                            ),
-                            iconSize: 20,
-                            padding: EdgeInsets.zero,
-                          ),
-                      ),
-                    ],
-                  );
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          pet.petName,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 24,
+          width: 24,
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.favorite_border_outlined,
+            ),
+            iconSize: 20,
+            padding: EdgeInsets.zero,
+          ),
+        ),
+      ],
+    );
   }
 
   Positioned backButton(BuildContext context) {
     return Positioned(
-          top: 40,
-          left: 16,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    spreadRadius: 1,
-                    blurRadius: 4,
-                  ),
-                ],
+      top: 40,
+      left: 16,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 1,
+                blurRadius: 4,
               ),
-              child: Icon(
-                Icons.arrow_back_ios_new,
-                color: Colors.black.withOpacity(0.8),
-                size: 30,
-              ),
-            ),
+            ],
           ),
-        );
+          child: Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.black.withOpacity(0.8),
+            size: 30,
+          ),
+        ),
+      ),
+    );
   }
 
   Positioned backgroundImage() {
     return Positioned.fill(
-          child: Image.asset(
-            pet.petIconPath,
-            fit: BoxFit.cover,
-          ),
-        );
+      child: Image.asset(
+        pet.petIconPath,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
